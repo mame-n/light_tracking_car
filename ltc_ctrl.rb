d111a45ca569cfb31ddefd231d7837fcf955038d
Shouichi Nakauchi <nakauchi.s@gmail.com>
2020-03-29 18:52:26 +0900

    Executable

--- ltc_ctrl.rb
+++ ltc_ctrl.rb
@@ -1,38 +1,147 @@
-# echo 23 > /sys/class/gpio/export
-# echo 23 > /sys/class/gpio/unexport
+GPIO_path = "/sys/class/gpio/"
+BCM16 = 16
+BCM17 = 17
+BCM18 = 18
+BCM19 = 19
+BCM20 = 20
+BCM26 = 26
+BCM27 = 27
 
-gpio_path = "/sys/class/gpio/"
-export_path = gpio_path + "export"
-unexport_path = gpio_path + "unexport"
+Light_center = BCM17
+Light_left = BCM18
+Light_right = BCM27
 
-begin
-  File.open( export_path, "w" ) do |fp|
-    fp.write("23")
+Motor_Left_Forward = BCM19
+Motor_Left_Reverse = BCM26
+Motor_Right_Forward = BCM16
+Motor_Right_Reverse = BCM20
+
+class LightTrackingCar
+  def initialize
+    open_gpios()
+
+    @light_bcms = [Light_right, Light_center, Light_left]
+    @motor_bcms = [Motor_Left_Forward, Motor_Left_Reverse, Motor_Right_Forward, Motor_Right_Reverse]
+    set_GPIO_direction
+
+    @index_Cds = [4,2,1]
+
   end
-  
-rescue Errno::EBUSY
-  puts "EBUSY"
-  File.open( unexport_path, "w" ) do |fp|
-    fp.write( "23" )
+
+  def open_gpios()
+    [BCM16,BCM17,BCM18,BCM19,BCM20,BCM26,BCM27].each do |gpio|
+      open_gpio( gpio )
+    end
   end
-  retry
 
-rescue => e
-  puts "Exception!!  #{e.message}"
+  def open_gpio( gpio_num )
+    export_path = GPIO_path + "export"
+    unexport_path = GPIO_path + "unexport"
+
+    begin
+      File.open( export_path, "w" ) do |fp|
+        fp.write( gpio_num.to_s )
+      end
+      
+    rescue Errno::EBUSY
+      puts "EBUSY"
+      File.open( unexport_path, "w" ) do |fp|
+        fp.write( gpio_num.to_s )
+      end
+      retry
+    
+    rescue => e
+      puts "Exception!!  #{e.message}"
+    end
+  end
+
+  def set_GPIO_direction
+    @light_bcms.each do |bcm|
+      direction_path, value_path = set_path( bcm )
+
+      File.open( direction_path, "w" ) do |fp|
+        fp.write( "in" )
+      end
+    end
+
+    @motor_bcms.each do |bcm|
+      direction_path, value_path = set_path( bcm )
+
+      File.open( direction_path, "w" ) do |fp|
+        fp.write( "out" )
+      end
+    end
+  end
+ 
+  def status_Cds
+    @light_bcms.inject(0) do |sum_light, light_bcm|
+#      puts "sum : #{sum_light}, bcm : #{light_bcm}"
+      if is_light_on?( light_bcm )
+        value = 2 ** @light_bcms.index(light_bcm)
+        puts "***sum : #{sum_light}, bcm : #{light_bcm}, val : #{value}"
+        sum_light + value
+      else
+        sum_light
+      end
+    end
+  end
+
+  def to_Cds_index( bcm )
+    @index_Cds[@light_bcms.index(bcm)]
+  end
+
+  def set_path( bcm )
+    base_path = GPIO_path + "gpio" + sprintf( "%d", bcm.to_s ) + "/"
+    [base_path + "direction", base_path + "value"]
+  end
+
+  def is_light_on?( direction )
+    direction_path, value_path = set_path( direction )
+#    puts "dir : #{direction_path}, val : #{value_path}"
+
+    result = 0
+    File.open( value_path, "r" ) do |fp|
+      result = fp.read.to_i
+    end
+
+#    puts "result #{result}"
+
+    result == 1 ? true : false
+  end
+
+  def logic_table
+    [ # bit assingment is as follows
+      # left forward, left reverse, right forward, right reverse
+      [0,0,0,0], # All lights are off
+      [1,0,0,1], # immediately right
+      [1,0,1,0], # forward
+      [1,0,0,0], # simple left
+      [0,1,1,0], # immediately left
+      [0,0,0,0], # Stop
+      [0,0,1,0], # simple right
+      [0,0,0,0], # Stop
+    ]
+  end
+
+  def main
+    while 1
+      puts "Logic #{logic_table[status_Cds]}"
+      logic_table[status_Cds].each_with_index do |bit, idx|
+        motor_on(idx) if bit == 1
+      end
+      sleep(1)
+    end
+  end
+
+  def motor_on( idx )
+    direction_path, value_path = set_path( @motor_bcms[idx] )
+
+    File.open( value_path, "w" ) do |fp|
+      fp.write( "1" )
+    end
+  end
 end
 
-direction_path = gpio_path + "gpio23/direction"
-value_path = gpio_path + "gpio23/value"
-
-File.open( direction_path, "w" ) do |fp|
-  fp.write( "out" )
+if $0 == __FILE__
+  LightTrackingCar.new.main
 end
-
-File.open( value_path, "w" ) do |fp|
-  fp.write( "1" )
-end
-
-File.open( unexport_path, "w" ) do |fp|
-  fp.write( "23" )
-end
-
---
+++ t_ltc_ctrl.rb
@@ -0,0 +1,45 @@
+require "test/unit"
+require "./ltc_ctrl.rb"
+
+class TC < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_set_path
+    o = LightTrackingCar.new
+    assert_equal( ["/sys/class/gpio/gpio23/direction", "/sys/class/gpio/gpio23/value"], o.set_path( 23 ))
+    assert_equal( ["/sys/class/gpio/gpio4/direction", "/sys/class/gpio/gpio4/value"], o.set_path( 4 ))
+  end
+
+  def test_is_light_on
+    o = LightTrackingCar.new
+    assert_equal( nil, o.is_light_on?( 18 ))
+  end
+
+  def test_open_gpios
+    o = LightTrackingCar.new
+    assert_equal( false, o.open_gpios )
+  end
+
+  def test_to_Cds_index
+    o = LightTrackingCar.new
+    assert_equal( 2, o.to_Cds_index( 17 ))
+    assert_equal( 4, o.to_Cds_index( 18 ))
+    assert_equal( 1, o.to_Cds_index( 27 ))
+  end
+
+  def test_status_Cds
+    o = LightTrackingCar.new
+    assert_equal( 0, o.status_Cds )
+  end
+
+  def test_motor_on
+    o = LightTrackingCar.new
+    assert_equal( nil, o.motor_on(3) )
+  end
+
+  def test_main
+    o = LightTrackingCar.new
+    assert_equal( nil, o.main )
+  end
+end
